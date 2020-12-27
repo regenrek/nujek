2c712c5e58de7ebd3b49456d636341946484a443
Kevin Regenrek <hello@regenrek.at>
2020-12-27 21:41:24 +0100

    update store function

--- packages/ui/plugins/init-store.js
+++ packages/ui/plugins/init-store.js
@@ -1,27 +1,6 @@
-import { state, mutations, getters, actions } from '../store/nav'
+import { registerNavStore } from '../store/nav'
 
 export default (context) => {
   const { store } = context
-
-  store.registerModule(
-    'nav',
-    {
-      namespaced: true,
-      state: () => ({
-        navOpen: false
-      }),
-      actions: {
-        toggle({ commit }) {
-          commit('toggle')
-        }
-      },
-      mutations: {
-        toggle(state) {
-          state.navOpen = !state.navOpen
-        }
-      },
-      getters: {}
-    },
-    { preserveState: !!store.state['nav'] }
-  )
+  registerNavStore(store)
 }
--- packages/ui/store/nav.js
+++ packages/ui/store/nav.js
@@ -1,41 +1,57 @@
-const types = {
-  SET_NAV: 'SET_NAV',
-  TOGGLE_NAV: 'TOGGLE_NAV'
-}
-
-const state = () => ({
-  navOpen: false
-})
-
-// getters
-const getters = {}
-
-const mutations = {
-  [types.SET_NAV](state, status) {
-    state.navOpen = status
-  },
-  [types.TOGGLE_NAV](state) {
-    state.navOpen = !state.navOpen
-  }
-}
-const actions = {
-  openNav({ commit }) {
-    commit(types.SET_NAV, true)
-  },
-  closeNav({ commit }) {
-    commit(types.SET_NAV, false)
-  },
-  setNav({ commit }, active) {
-    commit(types.SET_NAV, active)
-  },
-  toggle({ commit }) {
-    commit(types.TOGGLE_NAV)
-  }
-}
-
-export default {
-  state,
-  getters,
-  actions,
-  mutations
+export const registerNavStore = (store) => {
+  store.registerModule(
+    'nav',
+    {
+      namespaced: true,
+      state: () => ({
+        navOpen: false,
+        settings: {}
+      }),
+      actions: {
+        toggle({ commit }) {
+          commit('toggle')
+        },
+        setSettings({ commit }, payload) {
+          commit('setSettings', payload)
+        },
+        open({ commit }) {
+          commit('set', true)
+        },
+        close({ commit }) {
+          commit('set', false)
+        }
+      },
+      mutations: {
+        toggle(state) {
+          state.navOpen = !state.navOpen
+        },
+        set(state, payload) {
+          state.navOpen = payload
+        },
+        setSettings(state, payload) {
+          state.settings = payload
+        }
+      },
+      getters: {
+        mobile(state) {
+          return state.settings.mobile_navigation
+            ? state.settings.mobile_navigation
+            : state.settings.navigation
+        },
+        mobileSub(state) {
+          return state.settings.mobile_subnavigation
+        },
+        main(state) {
+          return state.settings.navigation
+        },
+        sub(state) {
+          return state.settings.subnavigation
+        },
+        footer(state) {
+          return state.settings.footer_navigation
+        }
+      }
+    },
+    { preserveState: !!store.state['nav'] }
+  )
 }
