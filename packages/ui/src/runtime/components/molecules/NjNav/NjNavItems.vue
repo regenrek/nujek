287c234da85878a5b1b85bee73b04daf3a772e3c
regenrek <hello@regenrek.at>
2021-07-28 16:27:22 +0200

    update njnavitems with more features

--- packages/ui/src/runtime/components/molecules/NjNav/NjNavItems.vue
+++ packages/ui/src/runtime/components/molecules/NjNav/NjNavItems.vue
@@ -1,32 +1,49 @@
 <template>
-  <nav>
+  <nav :class="getThemeClass('nav')">
     <slot
-      v-for="navItem in items"
+      v-for="(navItem, index) in items"
       v-bind="{
+        index,
         linkTo: $sbutils.linkTo(navItem),
         tag: $sbutils.tag(navItem),
         label: $sbutils.label(navItem),
+        navItem
       }"
+      :class="getThemeClass('navItems')"
     >
       <NjNavItem
         :link-to="$sbutils.linkTo(navItem)"
         :tag="$sbutils.tag(navItem)"
         :label="$sbutils.label(navItem)"
-        class="cursor-pointer hover:text-gray-400"
+        :class="getThemeClass('navItem')"
       />
     </slot>
   </nav>
 </template>
 
 <script>
-export default {
+import Component from '../../../base/Component'
+const NjNavItems = Component.extend({
+  name: 'NjNavItems',
   props: {
     items: {
       type: Array,
       default () {
         return []
       }
+    },
+    classes: {
+      type: Object,
+      default () {
+        return {
+          nav: '',
+          navItems: '',
+          navItem: 'cursor-pointer hover:text-gray-400'
+        }
+      }
     }
   }
-}
+})
+
+export default NjNavItems
 </script>
