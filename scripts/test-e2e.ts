bb304f139f9875a6246929c7ffa92c6b878a7c92
regenrek <hello@regenrek.at>
2021-09-20 22:24:23 +0200

    fix scripts

---
+++ scripts/test-e2e.ts
@@ -0,0 +1,18 @@
+import { execSync } from 'child_process'
+import path from 'path'
+import consola from 'consola'
+import { activePackages } from './packages'
+
+const args = process.argv.slice(2)
+run()
+
+function run() {
+    for (const { name } of activePackages) {
+        execSync(`pnpm test:e2e`, {
+            stdio: 'inherit',
+            cwd: path.join('packages', name)
+        })
+    }
+}
+
+export { run as lint }
