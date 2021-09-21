92931c925ec77e9608e1952424f1b4c014be5b3a
regenrek <hello@regenrek.at>
2021-09-21 12:09:56 +0200

    update tests to work with pipeline

--- packages/ui/test/e2e/ssr.spec.ts
+++ packages/ui/test/e2e/ssr.spec.ts
@@ -1,25 +1,16 @@
-import { setupTest, createPage, url } from '@nuxt/test-utils'
-import type { Page } from 'playwright'
+import { setupTest, createPage } from '@nuxt/test-utils'
 
 describe('browser (ssr:true)', () => {
-  jest.setTimeout(60000)
-
   setupTest({
     browser: true,
-    rootDir: 'test/fixtures'
+    rootDir: 'test/fixtures',
+    setupTimeout: 120000
   })
-  let page: Page
-
-  //   const requests: string[] = []
 
   test('should start', async () => {
-    page = await createPage()
-    // page.route('**', (route) => {
-    //   requests.push(route.request().url())
-    //   return route.continue()
-    // })
-    page.goto(url('/'))
-    const body = await page.innerHTML('body')
-    expect(body).toContain('Todo')
+    const page = await createPage('/')
+    const html = await page.innerHTML('body')
+
+    expect(html).toContain('Todo')
   })
 })
