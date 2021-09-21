f1cb2772211641f7b7e818f60d9a20e365a1498a
regenrek <hello@regenrek.at>
2021-09-21 10:51:13 +0200

    increase timeout

--- packages/ui/test/e2e/ssr.spec.ts
+++ packages/ui/test/e2e/ssr.spec.ts
@@ -2,14 +2,22 @@ import { setupTest, createPage, url } from '@nuxt/test-utils'
 import type { Page } from 'playwright'
 
 describe('browser (ssr:true)', () => {
+  jest.setTimeout(60000)
+
   setupTest({
     browser: true,
     rootDir: 'test/fixtures'
   })
   let page: Page
 
+  //   const requests: string[] = []
+
   test('should start', async () => {
     page = await createPage()
+    // page.route('**', (route) => {
+    //   requests.push(route.request().url())
+    //   return route.continue()
+    // })
     page.goto(url('/'))
     const body = await page.innerHTML('body')
     expect(body).toContain('Todo')
