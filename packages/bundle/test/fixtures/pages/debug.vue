00024dd8cca6e721baed3fc43a8d10a6611e827d
regenrek <hello@regenrek.at>
2021-09-22 21:27:28 +0200

    update to yarn and fix bundle

---
+++ packages/bundle/test/fixtures/pages/debug.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="index">
+    <Blok class="blok" :blok="story.content" />
+  </div>
+</template>
+
+<script>
+import { story } from './../../__mocks__/story_debug'
+export default {
+  data () {
+    return {
+      story
+    }
+  }
+}
+</script>
